f02db0674d1633e09b95c5e449172d4eb25dabcb
dhanyanipin <dhanyapulukkole@gmail.com>
2023-09-15 17:03:20 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+
+var user=document.getElementById("user");
+var password=document.getElementById("password");
+// var click=document.getElementById("click");
+var para1=document.getElementById("para1");
+
+function validate(){
+   
+
+
+
+// alert("button clicked");
+
+function display1(){
+    para1.innerHTML="field is empty";
+   return false;
+    
+}
+function display2(){
+    para1.innerHTML="Username and password is correct";
+    
+        return true;
+  
+    
+}
+function display3(){
+    para1.innerHTML="Username or password is incorrect";
+   return false;
+    
+    
+}
+function validation(a,b,callback1,callback2,callback3){
+
+    let newuser = {
+        username:"admin",
+        password: 12345,
+      };
+if(a=='' || b==''){
+    callback1();
+}
+else{
+if (a==newuser.username && b==newuser.password) {
+    callback2();
+}
+else{
+    callback3();
+
+}
+}
+
+
+}
+
+validation(user.value,password.value,display1,display2,display3);
+}
